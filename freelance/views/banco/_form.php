be29a70f8125332bb927240a0724bca49988c52a
Mateo Ferrer <mateoferrer66@gmail.com>
2025-09-15 18:10:49 -0500

    v0.002

---
+++ freelance/views/banco/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Banco $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="banco-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'ban_nombre')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'ban_numcuenta')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'ban_eliminado')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ freelance/views/banco/index.php
@@ -0,0 +1,54 @@
+<?php
+
+use app\models\Banco;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\BancoSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Bancos';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="banco-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Banco', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        //'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            [
+                'attribute' => 'ban_id',
+                'label' => 'Codigo',
+            ],
+            [
+                'attribute' => 'ban_nombre',
+                'label' => 'Banco',
+            ],
+            [
+                'attribute' => 'ban_numcuenta',
+                'label' => '#_cuenta',
+            ],
+            [
+                'class' => ActionColumn::className(),
+                'header' => 'Acciones', //
+                'urlCreator' => function ($action, Banco $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'ban_id' => $model->ban_id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
